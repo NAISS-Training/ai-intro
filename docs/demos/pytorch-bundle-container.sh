bcb8b3e6a4799dca2d37c7c6228286d906af6336
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2026-02-04 13:11:02 +0100

    Add container recipes and runtime scripts

---
+++ docs/demos/pytorch-bundle-container.sh
@@ -0,0 +1,10 @@
+# This script is used for the Open Ondemand portal.
+# You can use it as a reference for creating a custom ~/portal/jupyter/*.sh file
+
+ml purge  # Ensure we don't have any conflicting modules loaded
+
+# The container must contain jupyter
+container=~/containers/pytorch-bundle.sif
+
+# You can launch jupyter notebook or lab, but you must specify the config file as below: 
+apptainer exec --nv $container jupyter lab --config="${CONFIG_FILE}" 
